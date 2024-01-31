4416e83fde21924b4a2d590bd477751c30aac7a1
mohan <ms4157801@gmail.com>
2024-01-31 20:59:19 +0545

    Initial commit

---
+++ library/models.py
@@ -0,0 +1,28 @@
+# library/models.py
+from django.db import models
+
+class User(models.Model):
+    UserID = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=255)
+    Email = models.EmailField(unique=True)
+    MembershipDate = models.DateField()
+
+class Book(models.Model):
+    BookID = models.AutoField(primary_key=True)
+    Title = models.CharField(max_length=255)
+    ISBN = models.CharField(max_length=13, unique=True)
+    PublishedDate = models.DateField()
+    Genre = models.CharField(max_length=50)
+
+class BookDetails(models.Model):
+    DetailsID = models.AutoField(primary_key=True)
+    BookID = models.OneToOneField(Book, on_delete=models.CASCADE)
+    NumberOfPages = models.IntegerField()
+    Publisher = models.CharField(max_length=255)
+    Language = models.CharField(max_length=50)
+
+class BorrowedBooks(models.Model):
+    UserID = models.ForeignKey(User, on_delete=models.CASCADE)
+    BookID = models.ForeignKey(Book, on_delete=models.CASCADE)
+    BorrowDate = models.DateField()
+    ReturnDate = models.DateField(null=True, blank=True)
